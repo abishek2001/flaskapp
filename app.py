cdf4cbd6855e85ceee7cc583c5532e1280d7eefa
Abishek Mahesh <70111686+abishek2001@users.noreply.github.com>
2024-01-25 01:00:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import requests  # Import requests library
 
 app = Flask(__name__)
+CORS(app, resources={r"/process": {"origins": ["https://genai-main-poc.web.app/", "http://localhost:3000/"]}})
 CORS(app, resources={r"/process": {"origins": "*"}})
 
 
@@ -12,17 +13,14 @@ chatgpt_headers = {
     "Content-Type": "application/json",
     "x-functions-key": "q0j51tgxCOqqg2sBLyTV41CeTxKt5hmCh1lqSAxb7IEDAzFu7gRl3g=="
 }
-
 # Initialize conversation history as an empty list
 conversation_history = []
-
 def get_chatgpt_completion(conversation):
     response = requests.post(chatgpt_url, json=conversation, headers=chatgpt_headers)
     return response.json()
-
 @app.route('/process', methods=['POST'])
 def process():
-    user_input = "hi"
+    user_input = request.json.get('user_input')
     # Check if the user input is the completion trigger
     if user_input.strip().lower() == "i have completed viewing the video":
         response = "Great! If you have any more questions in the future, feel free to ask."
@@ -34,23 +32,19 @@ def process():
             conversation_history.append([])  # Start a new conversation if there's none
         # Add user message to the current conversation
         conversation_history[-1].append({"role": "user", "content": user_input})
-
         # Get chatbot response based on the entire conversation history
         chatgpt_request = {'course_structure': {'skill': 'Business Communication', 'topics': ["Introduction to business communication","Basic communication principles","Effective verbal communication","Active listening skills","Written communication skills","Non-verbal communication","Professional email etiquette"]},
                            'conv_history': conversation_history}
         chatgpt_response = get_chatgpt_completion(chatgpt_request)
         response = chatgpt_response.get("message")
         function_call = chatgpt_response.get("function_call")
-
         # Check if a function call is received, and perform redirection
         if function_call:
             response ="Redirect"
             conversation_history.append([])
         # Add chatbot message to the current conversation
         conversation_history[-1].append({"role": "assistant", "content": response})
-
     print(response)
     return jsonify({"response": response})
-
 if __name__ == '__main__':
     app.run(debug=True)
