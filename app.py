6d1f21259f76c6ffa6d6b96edaec419ec7282129
Abishek Mahesh <70111686+abishek2001@users.noreply.github.com>
2024-03-06 11:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify,  redirect, session
 from flask_cors import CORS
-import requests
-
+import requests  # Import requests library
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Change this to a secure secret key
+app.secret_key = "disprz123"
 CORS(app, resources={r"/process": {"origins": ["https://genai-main-poc.web.app/", "http://localhost:3000/"]}})
+CORS(app, resources={r"/process": {"origins": "*"}})
 CORS(app, resources={r"/get_welcome_message": {"origins": ["https://genai-main-poc.web.app/", "http://localhost:3000/"]}})
+CORS(app, resources={r"/get_welcome_message": {"origins": "*"}})
+# Define the endpoint URL and headers for the ChatGPT model
 chatgpt_url = "http://34.93.3.215:8000/chat_conversation"
-
+# chatgpt_headers = {
+#     "Content-Type": "application/json",
+#     "x-functions-key": "q0j51tgxCOqqg2sBLyTV41CeTxKt5hmCh1lqSAxb7IEDAzFu7gRl3g=="
+# }
+# Initialize conversation history as an empty list
+conversation_history = []
+# endpoint_url = "http://34.93.3.215:8000/"
+# response = requests.get(endpoint_url)
+# endpoint_message = response.json()
+# conversation_history.append([]) 
+# conversation_history[-1].append({"role": "assistant", "content": endpoint_message})
 def get_chatgpt_completion(conversation):
     response = requests.post(chatgpt_url, json=conversation, timeout=10)
     return response.json()
-
 @app.route('/get_welcome_message', methods=['GET'])
 def get_welcome_message():
     try:
@@ -21,11 +32,8 @@ def get_welcome_message():
     except Exception as e:
         print(f"Error fetching data: {e}")
         return jsonify({"error": "Internal Server Error"}), 500
-
 @app.route('/process', methods=['POST'])
 def process():
-    user_input = request.json.get('user_input')
-
     # Check if the user input is the completion trigger
     if user_input.strip().lower() == "i have completed viewing the video":
         response = "Great! If you have any more questions in the future, feel free to ask."
@@ -52,7 +60,6 @@ def process():
     print(response)
     print("Conversation History: ")
     print(session['conversation_history'])
-    return jsonify({"response": response})
-
+    return jsonify({"response": response}
 if __name__ == '__main__':
     app.run(debug=True)
